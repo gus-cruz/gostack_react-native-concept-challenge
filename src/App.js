2a283f8e34e5ee06c2c864f60cdfc37339bd8492
Gustavo Ferreira <gustavofrc8@gmail.com>
2020-06-22 00:56:12 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,52 +1,125 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
 
 import api from './services/api';
-import "./styles.css";
 
-function App() {
+export default function App() {
   const [repositories, setRepositories] = useState([]);
 
   useEffect(() => {
-    api.get('/repositories').then(response => setRepositories(response.data));
+    api.get('/repositories').then(response => {
+      console.log(response.data);
+      setRepositories(response.data);
+    }).catch(error => console.log(error));
   }, []);
 
-  async function handleAddRepository() {
-    const response = await api.post('/repositories', {
-      title: `Novo projeto ${Date.now()}`,
-      url: "https://github.com/gustavofrc",
-      tech: ["Node.js", "ReactJS"]
-    });
-
-    const repository = response.data;
-    setRepositories([...repositories, repository]);
+  async function handleLikeRepository(id) {
+    api.post(`/repositories/${id}/like`).then(response => {
+      const newRepositories = repositories;
+      const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+      newRepositories[repositoryIndex].likes = response.data.likes;
+      setRepositories([...newRepositories]);   
+    })
   }
 
-  async function handleRemoveRepository(id) {
-    await api.delete(`/repositories/${id}`);
-
-    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
-    const newRepositories = repositories
-    newRepositories.splice(repositoryIndex, 1)
-    
-    setRepositories([...newRepositories]);
-  }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-          {repositories.map(repository => 
-          <li key={repository.id}>
-            {repository.title}
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+      <SafeAreaView style={styles.container}>
+      <FlatList data={repositories} keyExtractor={repository => repository.id}  renderItem={({ item: repository }) => (
+        <View style={styles.repositoryContainer}>
+          <Text style={styles.repository}>{repository.title}</Text>
 
-            <button onClick={() => handleRemoveRepository(repository.id)}>
-              Remover
-            </button>
-          </li>)}
-      </ul>
+          <View style={styles.techsContainer}>
+            {repository.techs.map(tech => {
+              return (
+                <Text style={styles.tech} key={`${tech}`}>
+                  {tech}
+                </Text>
+              )
+            })}
+          </View>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
+          <View style={styles.likesContainer}>
+            <Text
+              style={styles.likeText}
+              testID={`repository-likes-${repository.id}`}
+            >
+              {repository.likes == 1 ? `${repository.likes} curtida` : `${repository.likes} curtidas`}
+            </Text>
+          </View>
+
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => handleLikeRepository(repository.id)}
+            testID={`like-button-${repository.id}`}
+          >
+            <Text style={styles.buttonText}>Curtir</Text>
+          </TouchableOpacity>
+        </View>
+      )}/>
+        
+      </SafeAreaView>
+    </>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#7159c1",
+  },
+  repositoryContainer: {
+    marginBottom: 15,
+    marginHorizontal: 15,
+    backgroundColor: "#fff",
+    padding: 20,
+  },
+  repository: {
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  techsContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+  },
+  tech: {
+    fontSize: 12,
+    fontWeight: "bold",
+    marginRight: 10,
+    backgroundColor: "#04d361",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    color: "#fff",
+  },
+  likesContainer: {
+    marginTop: 15,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  likeText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  button: {
+    marginTop: 10,
+  },
+  buttonText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginRight: 10,
+    color: "#fff",
+    backgroundColor: "#7159c1",
+    padding: 15,
+  },
+});
